31463734dce554c578ddeae00eec309ede4e4cac
pedrosilva2703 <up201806526@fe.up.pt>
2023-01-19 11:28:36 +0000

    Register css completed

--- SIE_Proj2/pages/Non_Auth_user/register.php
+++ SIE_Proj2/pages/Non_Auth_user/register.php
@@ -42,55 +42,51 @@ if (!empty($_SESSION['sErrorMsg'])) {
 
 <body>
 
-	<form method="POST" action="../../action/Non_Auth_user/actionRegister.php">
+	<div class="content-title">Registo </div>
+	<div class="content-body">
+		<form method="POST" action="../../action/Non_Auth_user/actionRegister.php">
+			<table>
+				<tr>
+					<td>Email*</td>
+					<td><input type="text" value="teste" name="newuserEmail"></td>
+				</tr>
+				<tr>
+					<td>Password*</td>
+					<td><input type="text" name="newuserPassword"></td>
+				</tr>
+				<tr>
+					<td>Nome*</td>
+					<td><input type="text" name="newuserName"></td>
+				</tr>
+				<tr>
+					<td>Morada*</td>
+					<td><input type="text" name="newuserAddress"></td>
+				</tr>
+				<tr>
+					<td>Código Postal*</td>
+					<td><input type="text" name="newuserCP"></td>
+				</tr>
+				<tr>
+					<td>Contacto Telefónico</td>
+					<td><input type="text" name="newuserPhone"></td>
+				</tr>
+				<tr>
+					<td>NIF</td>
+					<td><input type="text" name="newuserNIF"></td>
+				</tr>
+				<tr>
+					<td class="auth-mandatory-text">*campos obrigatórios</td>
+					<td class="auth-error-text"> Mensagem de erro aqui </td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit" class="btn-form" value="Registar"></td>
+				</tr>
+			</table>
+		</form>
 
-		<h3>Registo</h3>
-
-
-		<table>
-			<tr>
-				<td>Email*</td>
-				<td><input type="text" value="teste" name="newuserEmail"></td>
-			</tr>
-			<tr>
-				<td>Password*</td>
-				<td><input type="text" name="newuserPassword"></td>
-			</tr>
-			<tr>
-				<td>Nome*</td>
-				<td><input type="text" name="newuserName"></td>
-			</tr>
-			<tr>
-				<td>Morada*</td>
-				<td><input type="text" name="newuserAddress"></td>
-			</tr>
-			<tr>
-				<td>Código Postal*</td>
-				<td><input type="text" name="newuserCP"></td>
-			</tr>
-			<tr>
-				<td>Contacto Telefónico</td>
-				<td><input type="text" name="newuserPhone"></td>
-			</tr>
-			<tr>
-				<td>NIF</td>
-				<td><input type="text" name="newuserNIF"></td>
-			</tr>
-			<tr>
-				<td>*Campos obrigatórios</td>
-				<td style="align:left"> Mensagem de erro aqui </td>
-			</tr>
-
-
-			<tr>
-				<td></td>
-				<td style="align:left"><input type="submit" value="REGISTAR"></td>
-			</tr>
-
-		</table>
-
-	</form>
-
+	</div>
+	
 	<?php
 		footer();
 	?>
