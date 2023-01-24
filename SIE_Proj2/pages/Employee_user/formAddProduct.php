7f6084c59966e6a22f353c86f77a51f979130381
pedrosilva2703 <up201806526@fe.up.pt>
2023-01-24 11:23:21 +0000

    New product page structured finished

--- SIE_Proj2/pages/Employee_user/formAddProduct.php
+++ SIE_Proj2/pages/Employee_user/formAddProduct.php
@@ -6,13 +6,15 @@
 <?php
 	menu();
 
-	$errorMsg = "";
-	$ref = "";
-	$name = "";
-	$address = "";
-	$cp = "";
-	$phone = "";
-	$nif = "";
+	$errorMsg       = "";
+	$ref            = "";
+	$name           = "";
+	$description    = "";
+	$subCategory    = "";
+	$price          = "";
+	$quantity       = "";
+    $discount       = "";
+    $highlight      = "";
 
 	if (!empty($_SESSION['sErrorMsg'])) {
 		//echo "<p class='error'>" . $_SESSION['sErrorMsg'] . "<p>";
@@ -40,39 +42,57 @@
 					<tr>
 						<td>Referência</td>
                         <td></td>
-						<td class=\"shorter-field\"><input  type=\"text\" value=\"".$ref."\" name=\"newProductRef\"></td>
+						<td class=\"shorter-field\"><input  type=\"number\" value=\"".$ref."\" name=\"newRef\"></td>
 					</tr>
 					<tr>
 						<td>Nome</td>
                         <td></td>
-						<td class=\"name-field\"><input  type=\"text\" value=\"".$ref."\" name=\"newProductRef\"></td>
+						<td class=\"name-field\"><input  type=\"text\" value=\"".$name."\" name=\"newName\"></td>
 					</tr>
 					<tr>
 						<td style=\"vertical-align:top\">Descrição</td>
                         <td></td>
-						<td><textarea class=\"description-field\"  name=\"description\" rows=\"10\" cols=\"77\"> P   </textarea>    </td>
+						<td><textarea class=\"description-field\"  name=\"description\" rows=\"10\" cols=\"77\" >".$description."</textarea>  </td>
 					</tr>
+                    <tr>
+                        <td>Subcategoria</td>
+                        <td></td>
+                        <td>
+                            <select name=\"newSubCategory\" class=\"add-cat-select\">
+                                <option class=\"selection_text\" value=\"\">    Selecionar ... </option>
+                                <optgroup label=\"Componentes\">
+                                    <option value=\"Processadores> Processadores </option>
+                                    <option value=\"Processadores> Processadores </option>
+                                </optgroup>
+                                <optgroup label=\"Componentes\">
+                                    <option value=\"Processadores> Processadores </option>
+                                    <option value=\"Processadores> Fontes de alimentação </option>
+                                </optgroup>
+                            </select>
+                        </td>
+                    </tr>
+                    
 					<tr>
 						<td>Preço</td>
                         <td style=\"text-align: right\">(€)</td>
-						<td class=\"shorter-field\"><input  type=\"text\" value=\"".$ref."\" name=\"newProductRef\"></td>
+						<td class=\"shorter-field\"><input  type=\"number\" value=\"".$price."\" name=\"newPrice\" min=0 step=\"0.01\"></td>
 					</tr>
 					<tr>
 						<td>Quantidade</td>
                         <td></td>
-						<td class=\"shorter-field\"><input  type=\"text\" value=\"".$ref."\" name=\"newProductRef\"></td>
+						<td class=\"shorter-field\"><input  type=\"number\" value=\"".$quantity."\" name=\"newQuantity\" min=0></td>
 					</tr>
 					<tr>
 						<td>Desconto</td>
                         <td style=\"text-align: right\">(%)</td>
-						<td class=\"shorter-field\"><input  type=\"text\" value=\"".$ref."\" name=\"newProductRef\"></td>
+						<td class=\"shorter-field\"><input  type=\"number\" value=\"".$discount."\" name=\"newDiscount\" min=0 max=100></td>
 					</tr>
 					<tr>
 						<td>Destaque</td>
                         <td></td>
 						<td>
-                            <input type=\"radio\" name=\"highlight\" value=1> Sim
-                            <input type=\"radio\" name=\"highlight\" value=0> Não
+                            <input type=\"radio\" name=\"newHighlight\" value=1 ".($highlight=="true" ? "checked" : "").   "> Sim
+                            <input type=\"radio\" name=\"newHighlight\" value=0 ".($highlight=="false" ? "checked" : "").  "> Não
                         </td>
 					</tr>
 					<tr>
