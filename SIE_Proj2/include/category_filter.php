885084324bbcca57488815d2c75522adc80ae23f
pedrosilva2703 <up201806526@fe.up.pt>
2023-01-26 12:30:00 +0000

    Cat and subcat filter refactored

--- SIE_Proj2/include/category_filter.php
+++ SIE_Proj2/include/category_filter.php
@@ -6,8 +6,48 @@
 <?php
     // Selection forms for filter in category and subcategory
 
-    function select_category(){
+    function select_category($categorySelected){
+        echo"   <select name=\"category\" class=\"emp-cat-select\" onchange=\"this.form.submit()\">
+                    <option value=\"\">Selecionar categoria:</option>
+                    <option value   =\"*\" ".($categorySelected=="*" ? "selected" : "").">Todas</option>";
 
+                // Show options with categories from DB
+                $category_result    = getAllCategories();
+                $category           = pg_fetch_assoc($category_result);
+
+                while (isset($category["nome"])) {
+                // Show option and set selected (if it was previously selected)
+                echo "
+                    <option value = '".$category['nome']."' ".($categorySelected==$category['nome'] ? "selected" : "")." >"
+                        .$category['nome']."
+                    </option>";
+                $category   =   pg_fetch_assoc($category_result);
+                }
+        echo"   </select>";
+    }
+
+    function select_subcategory($categorySelected, $subCategorySelected){
+        //If category was not selected or selected all, disable the subcategory selection box
+        $disable_select = ( ($categorySelected=="" || $categorySelected=="*") ? "disabled" : "");
+        echo"   <select name = \"subcategory\"    class = \"emp-cat-select\"  onchange = \"this.form.submit()\" ".$disable_select.">
+                    <option value   =   \"\" >  Selecionar subcategoria:    </option>";    
+
+        // If some category was selected, retrieve subcategories for that category
+        if($categorySelected!=""){
+            echo "  <option value   =\"*\" ".($subCategorySelected=="*" ? "selected" : "").">Todas</option>";
+
+            // Show options with subcategories from DB
+            $subCategory_result = getAllSubCategories($categorySelected);
+            $subCategory        = pg_fetch_assoc($subCategory_result);
+            while (isset($subCategory["nome"])) {
+                // Show option
+                echo "              <option value = '" . $subCategory['nome'] . "' ".($subCategorySelected==$subCategory['nome'] ? "selected" : "").">"
+                                        .$subCategory['nome']."
+                                    </option>";
+                $subCategory    = pg_fetch_assoc($subCategory_result);
+            }   
+        }
+        echo"   </select>";
     }
 
 ?>
--- SIE_Proj2/pages/Employee_user/f_ListOrders.php
+++ SIE_Proj2/pages/Employee_user/f_ListOrders.php
@@ -1,6 +1,7 @@
 <?php
     /* PHP includes */
     include_once("../../include/header.php");
+    include_once("../../include/category_filter.php");
     include_once ("../../database/order.php");
 ?>
 
@@ -26,61 +27,18 @@
                 <td>
                     <form  style= "margin: 0px" action="../../action/Employee_user/actionCategoryFilter.php" method="post" > 
                         <input type="text" name="page" value="f_ListOrders" hidden>
-                        <select name="category" class="emp-cat-select" onchange="this.form.submit()">
-                            <option value="">Selecionar categoria:</option>
-<?php
-    /**************************** Category Select Box ****************************/
-    
-    echo "                  <option value   =\"*\" ".($categorySelected=="*" ? "selected" : "").">Todas</option>";
-
-    // Show options with categories from DB
-    $category_result    = getAllCategories();
-    $category           = pg_fetch_assoc($category_result);
-    while (isset($category["nome"])) {
-        // Show option and set selected (if it was previously selected)
-        echo "              <option value = '".$category['nome']."' ".($categorySelected==$category['nome'] ? "selected" : "")." >"
-                                .$category['nome']."
-                            </option>";
-        $category       = pg_fetch_assoc($category_result);
-    }
-    echo "              </select>
+                        <?php   select_category($categorySelected);                         ?>
                     </form>
-                </td>";
-
-    /**************************** SubCategory Select Box ****************************/
-    
-    //If category was not selected or selected all, disable the subcategory selection box
-    $disable_select = ( ($categorySelected=="" || $categorySelected=="*") ? "disabled" : "");
-    echo"       <td>
-                    <form style= \"margin: 0px\" action=\"../../action/Employee_user/actionSubCategoryFilter.php\" method=\"post\">
-                        <input type=\"text\" name=\"page\" value=\"f_ListOrders\" hidden>
-                        <select name = \"subcategory\"    class = \"emp-cat-select\"  onchange = \"this.form.submit()\" ".$disable_select.">
-                            <option value   =   \"\" >  Selecionar subcategoria:    </option>";    
-
-    // If some category was selected, retrieve subcategories for that category
-    if($categorySelected!=""){
-        echo "                  <option value   =\"*\" ".($subCategorySelected=="*" ? "selected" : "").">Todas</option>";
-
-        // Show options with subcategories from DB
-        $subCategory_result = getAllSubCategories($categorySelected);
-        $subCategory        = pg_fetch_assoc($subCategory_result);
-        while (isset($subCategory["nome"])) {
-            // Show option
-            echo "              <option value = '" . $subCategory['nome'] . "' ".($subCategorySelected==$subCategory['nome'] ? "selected" : "").">"
-                                     .$subCategory['nome']."
-                                </option>";
-            $subCategory    = pg_fetch_assoc($subCategory_result);
-        }   
-    }
-
-    echo"               </select>
+                </td>
+                <td>
+                    <form style= "margin: 0px" action="../../action/Employee_user/actionSubCategoryFilter.php" method="post">
+                        <input type="text" name="page" value="f_ListOrders" hidden>
+                        <?php   select_subcategory($categorySelected, $subCategorySelected) ?>
                     </form>
-                </td>";
-?>
+                </td>
             </tr>
         </table>
         
-
 <?php
     /**************************** Show Filter Results ****************************/
     
